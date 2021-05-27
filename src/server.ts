3fa5be43f9011b18af5601297eb7094b6e10d9c4
ebertmota <ebert.mota03@gmail.com>
2021-05-27 20:44:33 -0300

    finishing import csv useCases

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './modules/cars/routes';
+import swaggerConfig from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+
 app.use(router);
 
 app.listen(3333, () => console.log('Server is running at 3333'));
