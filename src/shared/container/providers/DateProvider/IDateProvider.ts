51ef81c62ddbefe6bdc8be725ec45365d7bab844
ebertmota <ebert.mota03@gmail.com>
2021-09-30 09:54:25 -0300

    feat: add compreDays method

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,5 +1,6 @@
 export interface IDateProvider {
   compareHours(start_date: Date, end_date: Date): number;
+  compareDays(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
 }
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -17,6 +17,13 @@ class DayjsDateProvider implements IDateProvider {
     return dayjs(utc_end_date).diff(utc_start_date, 'hours');
   }
 
+  compareDays(start_date: Date, end_date: Date): number {
+    const utc_end_date = this.convertToUTC(end_date);
+    const utc_start_date = this.convertToUTC(start_date);
+
+    return dayjs(utc_end_date).diff(utc_start_date, 'days');
+  }
+
   convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
   }
